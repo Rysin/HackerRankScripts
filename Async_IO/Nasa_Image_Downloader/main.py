9346649e6665ab7fe25fe5d020c4b1879ab77826
Sourabh Vishwas Desai <sourabh301094@gmail.com>
2022-10-11 23:45:46 +0530

    Added image fetch function for NASA img lib.

--- Async_IO/Nasa_Image_Downloader/main.py
+++ Async_IO/Nasa_Image_Downloader/main.py
@@ -1,19 +1,54 @@
 import typer
+from io import BytesIO
+from datetime import date, datetime
+import httpx
+from config import api_url
+from PIL import Image
 
 # Create typer app
-
 sd = typer.Typer()
 
+COUNT :int  = 1
+today :str = str(date.today())
+image_formats = ['jpg', 'png', 'jpeg', 'tiff', 'bmp']
 
 @sd.command()
 def hello(name):
-    typer.echo(f'Hello {name}', color='red')
+    typer.echo(f'Hello {name}', color=True)
     return None
 
 
 @sd.command()
 def squareIt(num):
     typer.echo(f'Sqaure = {int(num) ** 2}')
+
+@sd.command()
+def fetchImage(date: datetime = today):
+    query = f"{api_url}&count={COUNT}"
+    print(f'QUERY : {query}')
+
+    resp = httpx.get(query)
+    print(f'status code : {resp.status_code}')
+    resp.raise_for_status()
+    data = resp.json()[-1]
+    print(f'DATA : {type(data)} \n {data}')
+
+    print(data.keys())
+
+    img_url = data.get('hdurl')
+    title = data.get('title')
+
+    img_name = img_url.split('//')[-1]
+    ext = img_name.split('.')[-1]
+
+    print(f'EXT : {ext}')
+
+    if ext not in image_formats:
+        print('Not An Image')
+    else:
+        img = httpx.get(img_url)
+        image = Image.open(BytesIO(img.content))
+        image.show()
 
 
 if __name__ == '__main__':
