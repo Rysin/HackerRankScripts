191a8d77e119f20b975ed57258094449d152f011
Sourabh Vishwas Desai <sourabh301094@gmail.com>
2023-10-23 17:37:36 +0530

    Added Requirement.txt file along with gitingore file

---
+++ company/ol.py
@@ -0,0 +1,10 @@
+def star_pattern_recursion(i):
+    if i == 0:
+        return None
+    else:
+        print(i*'X')
+        return star_pattern_recursion(i-1)
+
+
+if __name__ == '__main__':
+    star_pattern_recursion(5)
